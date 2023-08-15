7de4fda628ccc2db4de0d32539c18f2339f5bd88
Student 1 <93119664+WillBill275@users.noreply.github.com>
2023-08-15 13:50:38 +0500

    Update script.js

--- insect-catch-game/script.js
+++ insect-catch-game/script.js
@@ -1,3 +1,4 @@
+// script.js
 const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 const start_btn = document.getElementById('start-btn')
